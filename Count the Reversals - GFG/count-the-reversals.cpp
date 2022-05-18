53af8ea254dacf6235f6435af0dd9cf8dd93ba46
Anurag Singh <53122840+anuragsingh7827@users.noreply.github.com>
2022-05-18 22:12:15 +0530

    Added solution - LeetHub

--- Count the Reversals - GFG/count-the-reversals.cpp
+++ Count the Reversals - GFG/count-the-reversals.cpp
@@ -22,18 +22,16 @@ int countRev (string s)
     // your code here
     int n = s.length();
     if(n & 1) return -1;
-    
-    stack<char> pile;
+
     int invalidOpen = 0;
     int invalidClose = 0;
     for(int i = 0; i < n; i++){
-        if(s[i] == '{') pile.push('{');
+        if(s[i] == '{') invalidOpen++;
         else{
-            if(!pile.empty()) pile.pop();
+            if(invalidOpen > 0) invalidOpen--;
             else invalidClose++;
         }
     }
-    invalidOpen = pile.size();
 
     int ans = 0;
     invalidOpen & 1 ? ans += (invalidOpen / 2) + 1 : ans += invalidOpen / 2;
