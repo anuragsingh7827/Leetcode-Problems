f4f0c1911a9e1bfa513c1cc36cb7e1bc8088fc9c
Anurag Singh <53122840+anuragsingh7827@users.noreply.github.com>
2022-06-08 17:56:35 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.cpp
+++ Floyd Warshall - GFG/floyd-warshall.cpp
@@ -23,10 +23,6 @@ class Solution {
 	            }
 	        }
 	    }
-	    
-	    for(int i = 0; i < n; i++){
-	        for(int j = 0; j < n; j++) if(matrix[i][j] == 1e9) matrix[i][j] = -1;
-	    }
 	}
 };
 
