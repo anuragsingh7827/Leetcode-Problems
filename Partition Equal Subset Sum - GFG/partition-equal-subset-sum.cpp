8e47bd6afcfd78208237b1d9e6d1564447e9e9c4
Anurag Singh <53122840+anuragsingh7827@users.noreply.github.com>
2022-06-29 21:19:00 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -9,27 +9,18 @@ using namespace std;
 
 class Solution{
 public:
-    int solve(int arr[], int N, int target, int i, int** dp){
+    int solve(int ind, int tar, int arr[],vector<vector<int>> &dp){
+        if(tar == 0) return 1;
+        if(ind == 0) return tar == arr[0];
         
-        if(i == N){
-            if(target == 0) return 1;
-            return 0;
-        }
+        if(dp[ind][tar] != -1) return dp[ind][tar];
         
-        if(target == 0) return 1;
+        int notTake = solve(ind - 1,tar,arr,dp);
         
-        if(dp[i][target] != -1) return dp[i][target];
+        int take = 0;
+        if(tar >= arr[ind]) take = solve(ind- 1,tar - arr[ind],arr,dp);
         
-        int op1 = solve(arr,N,target,i + 1,dp);
-        
-        int op2 = 0;
-        if(target >= arr[i]) op2 = solve(arr,N,target - arr[i],i + 1,dp);
-        
-        if(op1 == 1) return dp[i][target] = op1;
-        else if(op2 == 1) return dp[i][target] = op2;
-        else return dp[i][target] = 0;
-        
-        
+        return dp[ind][tar] = notTake || take;
     }
     int equalPartition(int N, int arr[])
     {
@@ -37,24 +28,11 @@ public:
         int sum = 0;
         for(int i = 0; i < N; i++) sum += arr[i];
         
-        if(sum % 2 != 0) return 0;
+        if(sum & 1) return 0;
         
-        int** dp = new int*[N + 1];
-        for(int i = 0; i < N + 1; i++) dp[i] = new int[sum + 1];
+        vector<vector<int>> dp(N, vector<int>((sum / 2) + 1, -1));
         
-        for(int i = 0; i < N + 1; i++){
-            for(int j = 0; j < sum + 1; j++) dp[i][j] = -1;
-        }
-        
-        int ans = solve(arr,N,sum / 2,0,dp);
-        
-        for(int i = 0; i < N + 1; i++) delete[] dp[i];
-        
-        delete[] dp;
-        
-        return ans;
-        
-        
+        return solve(N - 1,sum / 2,arr,dp);
     }
 };
 
