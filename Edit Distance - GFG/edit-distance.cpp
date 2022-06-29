2429e8dc5fb6d73af676b7169eeaa0ff44495eb6
Anurag Singh <53122840+anuragsingh7827@users.noreply.github.com>
2022-06-29 20:17:35 +0530

    Added solution - LeetHub

--- Edit Distance - GFG/edit-distance.cpp
+++ Edit Distance - GFG/edit-distance.cpp
@@ -24,25 +24,27 @@ class Solution {
         int n = s.size();
         int m = t.size();
         
-        vector<vector<int>> dp(n + 1, vector<int> (m + 1, 0));
+        vector<int> prev(m + 1, 0), cur(m + 1, 0);
         
-        for(int j = 0; j <= m; j++) dp[0][j] = j;
-        for(int i = 0; i <= n; i++) dp[i][0] = i;
+        for(int j = 0; j <= m; j++) prev[j] = j;
+        prev[0] = 0;
         
         for(int i = 1; i <= n; i++){
+            cur[0] = i;
             for(int j = 1; j <= m; j++){
-                if(s[i - 1] == t[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+                if(s[i - 1] == t[j - 1]) cur[j] = prev[j - 1];
                 else{
-                    int op1 = 1 + dp[i][j - 1];
-                    int op2 = 1 + dp[i - 1][j];
-                    int op3 = 1 + dp[i - 1][j - 1];
+                    int op1 = 1 + cur[j - 1];
+                    int op2 = 1 + prev[j];
+                    int op3 = 1 + prev[j - 1];
                     
-                    dp[i][j] =  min(op1,min(op2,op3));
+                    cur[j] =  min(op1,min(op2,op3));
                 }
             }
+            prev = cur;
         }
         
-        return dp[n][m];
+        return prev[m];
     }
 };
 
