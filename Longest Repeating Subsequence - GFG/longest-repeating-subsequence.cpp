a44c0122e3beb9b721e676c30ea236a34f326d0c
Anurag Singh <53122840+anuragsingh7827@users.noreply.github.com>
2022-07-01 13:35:53 +0530

    Added solution - LeetHub

--- Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
@@ -5,23 +5,21 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
 	public:
+	    int solve(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
+            if(i == 0 || j == 0) return 0;
+            
+            if(dp[i][j] != -1) return dp[i][j];
+            
+            if(s1[i - 1] == s2[j - 1] && i != j) return dp[i][j] = 1 + solve(i - 1,j - 1,s1,s2,dp);
+            
+            return dp[i][j] = max(solve(i,j - 1,s1,s2,dp),solve(i - 1,j,s1,s2,dp));
+        }
 		int LongestRepeatingSubsequence(string str){
 		    // Code here
-		    int n = str.length();
-		    int** dp = new int*[n + 1];
-		    for(int i = 0; i < n + 1; i++) dp[i] = new int[n + 1];
+		    int n = str.size();
+		    vector<vector<int>> dp(n + 1, vector<int> (n + 1, -1));
 		    
-            for(int i = 0; i < n + 1; i++) dp[i][n] = 0;
-            for(int j = 0; j < n + 1; j++) dp[n][j] = 0;
-            
-            for(int i = n - 1; i >= 0; i--){
-                for(int j = n - 1; j >= 0; j--){
-                    if(str[i] != str[j] || (str[i] == str[j] && i == j)) dp[i][j] = max(dp[i][j + 1],dp[i + 1][j]);
-                    else if(str[i] == str[j] && i != j) dp[i][j] = 1 + dp[i + 1][j + 1];
-                }
-            }
-            
-            return dp[0][0];
+		    return solve(n,n,str,str,dp);
 		}
 
 };
