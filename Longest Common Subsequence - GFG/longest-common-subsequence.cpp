91eb1df7ae56979fc018150cb61aef6d35e4a731
Anurag Singh <53122840+anuragsingh7827@users.noreply.github.com>
2022-07-01 12:54:28 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -22,17 +22,18 @@ class Solution
     int lcs(int x, int y, string s1, string s2)
     {
         // your code here
-        vector<vector<int>> dp(x + 1, vector<int> (y + 1, 0));
+        vector<int> prev(y + 1, 0), cur(y + 1, 0);
         
         for(int i = 1; i <= x; i++){
             for(int j = 1; j <= y; j++){
-                if(s1[i - 1] == s2[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
-                else dp[i][j] = max(dp[i][j - 1],dp[i - 1][j]);
+                if(s1[i - 1] == s2[j - 1]) cur[j] = 1 + prev[j - 1];
+                else cur[j] = max(cur[j - 1],prev[j]);
             }
+            prev = cur;
         }
         
         
-        return dp[x][y];
+        return prev[y];
     }
 };
 
