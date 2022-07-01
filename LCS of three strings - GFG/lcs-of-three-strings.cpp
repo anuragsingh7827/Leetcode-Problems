a7a30dc7d73031cd8d11557f77341b6e6eda4412
Anurag Singh <53122840+anuragsingh7827@users.noreply.github.com>
2022-07-01 18:53:45 +0530

    Added solution - LeetHub

--- LCS of three strings - GFG/lcs-of-three-strings.cpp
+++ LCS of three strings - GFG/lcs-of-three-strings.cpp
@@ -18,19 +18,19 @@ int main()
 
 
 int solve(int i, int j, int k, string &a, string &b, string &c, vector<vector<vector<int>>> &dp){
-    if(i < 0 || j < 0 || k < 0) return 0;
+    if(i == 0 || j == 0 || k == 0) return 0;
     
     if(dp[i][j][k] != -1) return dp[i][j][k];
     
-    if(a[i] == b[j] && b[j] == c[k]) return dp[i][j][k] =  1 + solve(i - 1,j - 1,k - 1,a,b,c,dp);
+    if(a[i - 1] == b[j - 1] && b[j - 1] == c[k - 1]) return dp[i][j][k] =  1 + solve(i - 1,j - 1,k - 1,a,b,c,dp);
     
-    if(a[i] == b[j] && b[j] != c[k]){
+    if(a[i - 1] == b[j - 1] && b[j - 1] != c[k - 1]){
         return dp[i][j][k] = max(solve(i - 1,j - 1,k,a,b,c,dp),solve(i,j,k - 1,a,b,c,dp));
-    }else if(a[i] == c[k] && c[k] != b[j]){
+    }else if(a[i - 1] == c[k - 1] && c[k - 1] != b[j - 1]){
         return dp[i][j][k] = max(solve(i - 1,j,k - 1,a,b,c,dp),solve(i,j - 1,k,a,b,c,dp));
-    }else if(b[j] == c[k] && c[k] != a[i]){
+    }else if(b[j - 1] == c[k - 1] && c[k - 1] != a[i - 1]){
         return dp[i][j][k] = max(solve(i,j - 1,k - 1,a,b,c,dp),solve(i - 1,j,k,a,b,c,dp));
-    }else if(a[i] != b[j] && b[j] != c[k]){
+    }else if(a[i - 1] != b[j - 1] && b[j - 1] != c[k - 1]){
         int op1 = solve(i,j - 1,k - 1,a,b,c,dp);
         int op2 = solve(i - 1,j,k - 1,a,b,c,dp);
         int op3 = solve(i - 1,j - 1,k,a,b,c,dp);
@@ -41,7 +41,7 @@ int solve(int i, int j, int k, string &a, string &b, string &c, vector<vector<ve
 int LCSof3 (string A, string B, string C, int n1, int n2, int n3)
 {
     // your code here
-    vector<vector<vector<int>>> dp(n1, vector<vector<int>> (n2, vector<int> (n3 , -1)));
+    vector<vector<vector<int>>> dp(n1 + 1, vector<vector<int>> (n2 + 1, vector<int> (n3 + 1, -1)));
     
-    return solve(n1 - 1,n2 - 1,n3 - 1,A,B,C,dp);
+    return solve(n1,n2,n3,A,B,C,dp);
 }
