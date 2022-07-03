fb0678a8b0b4d01a9ce1c9963acad5085f3c58f5
Anurag Singh <53122840+anuragsingh7827@users.noreply.github.com>
2022-07-04 02:08:58 +0530

    Added solution - LeetHub

--- Reach a given score - GFG/reach-a-given-score.cpp
+++ Reach a given score - GFG/reach-a-given-score.cpp
@@ -8,7 +8,7 @@ using namespace std;
 // Complete this function
 long long solve(int ind, int tar, vector<int> &scores, vector<vector<int>> &dp){
     if(tar == 0) return 1;
-    if(ind == 0) return tar % scores[ind] == 0;
+    if(ind == 0) return tar % scores[0] == 0;
 
     if(dp[ind][tar] != -1) return dp[ind][tar];
 
@@ -22,9 +22,23 @@ long long int count(long long int n)
 {
     vector<int> scores{3,5,10};
     
-    vector<vector<int>> dp(3, vector<int> (n + 1, -1));
+    vector<vector<int>> dp(3, vector<int> (n + 1, 0));
     
-    long long ans = solve(2,n,scores,dp);
+    for(int ind = 0; ind <= 2; ind++) dp[ind][0] = 1;
+    
+    for(int tar = 0; tar <= n; tar++) dp[0][tar] = tar % scores[0] == 0;
+    
+    for(int ind = 1; ind <= 2; ind++){
+        for(int tar = 1; tar <= n; tar++){
+            long long notTake = dp[ind - 1][tar];
+            long long take = 0;
+            if(tar >= scores[ind]) take = dp[ind][tar - scores[ind]];
+            
+            dp[ind][tar] = notTake + take;
+        }
+    }
+    
+    long long ans = dp[2][n];
     return ans;
 }
 
