e23ad1a708a849e54ffc52b7f6c9c014f526f8ac
Anurag Singh <53122840+anuragsingh7827@users.noreply.github.com>
2022-07-04 15:00:15 +0530

    Added solution - LeetHub

--- Word Break - GFG/word-break.cpp
+++ Word Break - GFG/word-break.cpp
@@ -12,38 +12,28 @@ using namespace std;
 // B : vector of available strings
 
 class Solution
-{   
-    // bool exist(string word, vector<string> B){
-    //     int m = B.size();
-    //     for(int i = 0; i < m; i++){
-    //         if(B[i] == word) return true;
-    //     }
-    //     return false;
-    // }
-    int solve(string A, int i, vector<string> B, int n, unordered_map<string,bool> h){
+{
+public:
+    bool solve(string s, int i, int n, unordered_map<string,bool> exist){
+        if(i == n) return true;
         
-        if(A[i] == '\0') return true;
-        
-        
-        string word = "";
+        string temp = "";
         for(int j = i; j < n; j++){
-            word.push_back(A[j]);
-            if(h[word] && solve(A,j + 1,B,n,h)) return true;
+            temp.push_back(s[j]);
+            if(exist[temp] && solve(s,j + 1,n,exist)) return true;
         }
+        
         return false;
     }
-public:
     int wordBreak(string A, vector<string> &B) {
         //code here
-        int n = A.length();
+        int n = A.size();
         int m = B.size();
         
-        unordered_map<string,bool> h;
-        for(int i = 0; i < m; i++) h[B[i]] = true;
+        unordered_map<string,bool> exist;
+        for(int i = 0; i < m; i++) exist[B[i]] = true;
         
-        if(solve(A,0,B,n,h)) return 1;
-        return 0;
-        
+        return solve(A,0,n,exist);
     }
 };
 
