1e74d506289d48779aaec1f18862bf4c2eee2d1b
Anurag Singh <53122840+anuragsingh7827@users.noreply.github.com>
2022-07-06 13:12:04 +0530

    Added solution - LeetHub

--- Geek and its Game of Coins - GFG/geek-and-its-game-of-coins.cpp
+++ Geek and its Game of Coins - GFG/geek-and-its-game-of-coins.cpp
@@ -6,6 +6,17 @@ using namespace std;
 class Solution{
 
 	public:
+	int solve(int n, int x, int y, vector<int> &dp){
+	    if(n == 0) return 0;
+	    if(n == 1) return 1;
+	    
+	    if(dp[n] != -1) return dp[n];
+	    
+	    if(n - 1 >= 0 && solve(n - 1,x,y,dp) == 0) return dp[n] = 1;
+	    else if(n - x >= 0 && solve(n - x,x,y,dp) == 0) return dp[n] = 1;
+	    else if(n - y >= 0 && solve(n - y,x,y,dp) == 0) return dp[n] = 1;
+	    return dp[n] = 0;
+	}
 	int findWinner(int N, int X, int Y)
 	{
 		// Your code goes here
@@ -16,9 +27,9 @@ class Solution{
 		
 		for(int i = 2; i <= N; i++){
 		    if(i - 1 >= 0 && dp[i - 1] == 0) dp[i] = 1;
-		    else if(i - X >= 0 && dp[i - X] == 0) dp[i] = 1;
-		    else if(i - Y >= 0 && dp[i - Y] == 0) dp[i] = 1;
-		    else dp[i] = 0;
+    	    else if(i - X >= 0 && dp[i - X] == 0) dp[i] = 1;
+    	    else if(i - Y >= 0 && dp[i - Y] == 0) dp[i] = 1;
+    	    else dp[i] = 0;
 		}
 		
 		return dp[N];
