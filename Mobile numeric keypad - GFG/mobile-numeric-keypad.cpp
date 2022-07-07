64852b6945d3c629f733398a61f32511c5d0ab66
Anurag Singh <53122840+anuragsingh7827@users.noreply.github.com>
2022-07-07 21:24:44 +0530

    Added solution - LeetHub

--- Mobile numeric keypad - GFG/mobile-numeric-keypad.cpp
+++ Mobile numeric keypad - GFG/mobile-numeric-keypad.cpp
@@ -17,6 +17,24 @@ class Solution{
 	    
 	    return dp[n][prev] = ways;
 	}
+	long long solve2(int n, int i, int j, int keypad[][3], vector<vector<vector<long long>>> &dp){
+	   	if(n == 0) return 1;
+	   	
+	   	if(dp[n][i][j] != -1) return dp[n][i][j];
+	   	
+	   	int di[5] = {0,-1,0,1,0};
+	    int dj[5] = {0,0,1,0,-1};
+	    
+	    long long ways = 0;
+	    for(int k = 0; k < 5; k++){
+	        int newI = i + di[k];
+	        int newJ = j + dj[k];
+	        if(newI < 0 || newI >= 4 || newJ < 0 || newJ >= 3 || keypad[newI][newJ] == -1) continue;
+	        ways += solve2(n - 1,newI,newJ,keypad,dp);
+	    }
+	    
+	    return dp[n][i][j] = ways;
+	}
 	long long getCount(int N)
 	{
 		// Your code goes here
@@ -30,23 +48,34 @@ class Solution{
 		int di[5] = {0,-1,0,1,0};
 		int dj[5] = {0,0,1,0,-1};
 		
-		vector<int>* adj = new vector<int> [10];
-		for(int i = 0; i < 4; i++){
-		    for(int j = 0; j < 3; j++){
-		        if(keypad[i][j] == -1) continue;
-		        for(int k = 0; k < 5; k++){
-		            int newI = i + di[k];
-		            int newJ = j + dj[k];
-		            if(newI < 0 || newI >= 4 || newJ < 0 || newJ >= 3 || keypad[newI][newJ] == -1) continue;
-		            adj[keypad[i][j]].push_back(keypad[newI][newJ]);
-		        }
-		    }
-		}
+// 		vector<int>* adj = new vector<int> [10];
+// 		for(int i = 0; i < 4; i++){
+// 		    for(int j = 0; j < 3; j++){
+// 		        if(keypad[i][j] == -1) continue;
+// 		        for(int k = 0; k < 5; k++){
+// 		            int newI = i + di[k];
+// 		            int newJ = j + dj[k];
+// 		            if(newI < 0 || newI >= 4 || newJ < 0 || newJ >= 3 || keypad[newI][newJ] == -1) continue;
+// 		            adj[keypad[i][j]].push_back(keypad[newI][newJ]);
+// 		        }
+// 		    }
+// 		}
 		
-		vector<vector<long long>> dp(N, vector<long long> (10, -1));
+// 		vector<vector<long long>> dp(N, vector<long long> (10, -1));
 		
+        // long long ans = 0;
+        // for(int i = 0; i <= 9; i++) ans += solve(N - 1,i,adj,dp);
+        
+        // return ans;
+        vector<vector<vector<long long>>> dp(N, vector<vector<long long>> (4, vector<long long> (3, -1)));
+        
         long long ans = 0;
-        for(int i = 0; i <= 9; i++) ans += solve(N - 1,i,adj,dp);
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 3; j++){
+                if(keypad[i][j] == -1) continue;   
+                ans += solve2(N - 1,i,j,keypad,dp);
+            }
+        }
         
         return ans;
 	}
