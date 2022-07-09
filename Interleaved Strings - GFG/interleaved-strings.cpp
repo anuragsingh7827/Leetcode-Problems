91b106de5c7bf805ee53f717f02dc661b915c7c5
Anurag Singh <53122840+anuragsingh7827@users.noreply.github.com>
2022-07-09 16:37:45 +0530

    Added solution - LeetHub

--- Interleaved Strings - GFG/interleaved-strings.cpp
+++ Interleaved Strings - GFG/interleaved-strings.cpp
@@ -10,50 +10,50 @@ using namespace std;
 class Solution{
   public:
     /*You are required to complete this method */
-bool solve(int i, int j, int k, string a, string b, string c, vector<vector<int>> &dp){
-        if(i == 0 && j >= 1){
-            while(j >= 1 && k >= 1){
-                if(b[j - 1] != c[k - 1]) return false;
+    bool solve(int i, int j, int k, string a, string b, string c, vector<vector<int>> &dp){
+        if(i < 0 && j >= 0){
+            while(j >= 0 && k >= 0){
+                if(b[j] != c[k]) return false;
                 j--;
                 k--;
             }
-            if(j == 0 && k == 0) return true;
+            if(j < 0 && k < 0) return true;
             return false;
         } 
-        if(j == 0 && i >= 1){
-            while(i >= 1 && k >= 1){
-                if(a[i - 1] != c[k - 1]) return false;
+        if(j < 0 && i >= 0){
+            while(i >= 0 && k >= 0){
+                if(a[i] != c[k]) return false;
                 i--;
                 k--;
             }
-            if(i == 0 && k == 0) return true;
+            if(i < 0 && k < 0) return true;
             return false;
         }
         
         if(dp[i][j] != -1) return dp[i][j];
         
-        if(a[i - 1] == c[k - 1] && b[j - 1] == c[k - 1]){
+        if(a[i] == c[k] && b[j] == c[k]){
             bool op1 = solve(i - 1,j,k - 1,a,b,c,dp);
             bool op2 = solve(i,j - 1,k - 1,a,b,c,dp);
             
             return dp[i][j] = op1 || op2;
         }
-        else if(a[i - 1] == c[k - 1]) return dp[i][j] = solve(i - 1,j,k - 1,a,b,c,dp);
-        else if(b[j - 1] == c[k - 1]) return dp[i][j] = solve(i,j - 1,k - 1,a,b,c,dp);
+        else if(a[i] == c[k]) return dp[i][j] = solve(i - 1,j,k - 1,a,b,c,dp);
+        else if(b[j] == c[k]) return dp[i][j] = solve(i,j - 1,k - 1,a,b,c,dp);
         
         else return dp[i][j] = false;
         
     }
-    bool isInterleave(string a, string b, string c) 
+    bool isInterleave(string A, string B, string C) 
     {
         //Your code here
-        int n = a.size();
-        int m = b.size();
-        int l = c.size();
+        int n = A.size();
+        int m = B.size();
+        int l = C.size();
         
-        vector<vector<int>> dp(n + 1, vector<int> (m + 1, -1));
+        vector<vector<int>> dp(n, vector<int> (m, -1));
         
-        return solve(n,m,l,a,b,c,dp);
+        return solve(n - 1,m - 1,l - 1,A,B,C,dp);
     }
 
 };
