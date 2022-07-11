eb6371fa4eb358241489ed84e190d275e844508d
Anurag Singh <53122840+anuragsingh7827@users.noreply.github.com>
2022-07-11 21:08:00 +0530

    Added solution - LeetHub

---
+++ Check if it is possible to survive on Island - GFG/check-if-it-is-possible-to-survive-on-island.cpp
@@ -0,0 +1,32 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int minimumDays(int S, int N, int M){
+        // code here
+        if(N * (S - (S / 7)) >= M * S) return ceil((M * S) / (N * 1.0));
+        return -1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int S, N, M;
+        cin>> S >> N >> M;
+        
+        Solution ob;
+        cout<<ob.minimumDays(S, N, M)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
