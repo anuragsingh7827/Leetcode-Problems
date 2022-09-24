5481559c59857e8e1e74c532e7a307ecd6636255
Anurag Singh <53122840+anuragsingh7827@users.noreply.github.com>
2022-09-24 17:20:34 +0530

    Time: 289 ms (35.29%), Space: 6.1 MB (22.46%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -4,14 +4,10 @@ public:
     int concatenatedBinary(int n) {
         
         long long ans = 0;
-        long long len = 0;
-        long long mul = 1;
-        for(long long i = n; i > 0; i--){
-            ans = ((ans % mod) + ((i % mod) * (mul % mod)) % mod) % mod;
-            int newLen = (floor(log2(i)) + 1);
-            for(int j = 0; j < newLen; j++){
-                mul = ((mul % mod) * (2 % mod)) % mod;
-            }
+        // long long mul = 1;
+        for(long long i = 1; i <= n; i++){
+            int len = floor(log2(i)) + 1;
+            ans = (((ans << len) % mod) + (i % mod)) % mod;
         }
         return ans;
     }
